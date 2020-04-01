b886132b3da72e1a0c609f4a1f09c77d06fab579
samuvel ramesh <62345838+samsharpy@users.noreply.github.com>
2020-04-01 20:15:27 +0530

    Add files via upload

---
+++ table program.py
@@ -0,0 +1,11 @@
+#craeted on ------
+#author :samuvel
+a=int(input('enter the number which table is needed')) #geting the table of which the multiples are needed
+c=1# dummy variable inzializing 1
+for num in range (1,100):#using for loop to range the output to 100
+      c=a*num
+      print(a, 'x' , num, '=',c)
+      # printing the muliples in 3x 1=3
+
+      
+    
